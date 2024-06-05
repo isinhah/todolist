c41c271aa6fbb55e89402a9674a0d5a24201f417
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-06-05 15:27:10 -0300

    implementacao do postgres

--- src/main/resources/db/migration/V1__create_table_tb_task.sql
+++ src/main/resources/db/migration/V1__create_table_tb_task.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS tb_task (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     title VARCHAR(255),
     description VARCHAR(255),
     category VARCHAR(255),
